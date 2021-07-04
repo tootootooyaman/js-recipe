cffbbd50e9dad90e5000000cc082e29c30c3ae75
tootootooyaman <toyama.a.ab@gmail.com>
2021-07-04 20:53:31 +0900

    FizzBuzzやったよ

---
+++ 04_ゼミ/week1/object.js
@@ -0,0 +1,14 @@
+const tooyama = {
+  name: "遠山哲",
+  furigana: "トオヤマアキラ",
+  age: 25,
+  sayHello: function() {
+    alert("こんばんは！！")
+  },
+  hobby: ["読書", "散歩", "プログラミング"],
+  isHuman: true,
+  parent: {
+    motherName: "Kuriko",
+    fatherName: "Tomoaki",
+  },
+}
